8e936ce298584dd39b94f9c855e5832101434c8d
praveen <mspraveenkumar77@gmail.com>
2024-07-30 06:56:14 +0000

    Updates

--- verify.php
+++ verify.php
@@ -5,19 +5,17 @@ require('config.php');
 session_start();
 
 require('razorpay-php-2.8.6/Razorpay.php');
+
 use Razorpay\Api\Api;
 use Razorpay\Api\Errors\SignatureVerificationError;
 
 $success = true;
 $error = "Payment Failed";
-
+print_r($_POST);
 if (empty($_POST['razorpay_payment_id']) === false) {
     $api = new Api($Key_Id, $Key_Secret);
 
     try {
-        // Please note that the razorpay order ID must
-        // come from a trusted source (session here, but
-        // could be database or something else)
         $attributes = array(
             'razorpay_order_id' => $_SESSION['razorpay_order_id'],
             'razorpay_payment_id' => $_POST['razorpay_payment_id'],
@@ -25,7 +23,7 @@ if (empty($_POST['razorpay_payment_id']) === false) {
         );
 
         $api->utility->verifyPaymentSignature($attributes);
-    } catch(SignatureVerificationError $e) {
+    } catch (SignatureVerificationError $e) {
         $success = false;
         $error = 'Razorpay Error : ' . $e->getMessage();
     }
@@ -49,19 +47,19 @@ if ($success === true) {
 try {
     $query = "INSERT INTO `razorpay-php`(`price`, `status`, `email`, `razorpay_order_id`, `razorpay_payment_id`, `razorpay_signature`) 
         VALUES (?, ?, ?, ?, ?, ?)";
-    
+
     $stmt = mysqli_prepare($connection, $query);
-    
+
     mysqli_stmt_bind_param($stmt, "ssssss", $_SESSION['price'], $status, $_SESSION['email'], $_SESSION['razorpay_order_id'], $_POST['razorpay_payment_id'], $_POST['razorpay_signature']);
-    
+
     $result = mysqli_stmt_execute($stmt);
-    
+
     if ($result) {
         echo "Data Recorded Successfully";
     } else {
         echo "Data Recording Failed";
     }
-    
+
     mysqli_stmt_close($stmt);
 } catch (Exception $e) {
     print_r($e);
